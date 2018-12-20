6b89a6b2a362f62abd14cbd6da0abf9d9ce3a450
Eli <elimaynard923@gmail.com>
2018-12-19 23:40:30 -0500

    More run (I am tired)

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+pkill gunicorn
 # Lots of workers because the majority of the blocking will be due to waiting for a response
-gunicorn --workers 12 --bind 0.0.0.0 wsgi
+gunicorn --workers 16 --bind 0.0.0.0 wsgi
